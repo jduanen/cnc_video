4e23983d0c1307db06f938f0a3d172af5df74b15
U-JDN-Laptop\jduanen <duane.northcutt@gmail.com>
2017-02-18 20:27:50 -0800

    Created.

---
+++ util.py
@@ -0,0 +1,18 @@
+"""X-Carve Interface -- Library"""
+
+
+# Merge a new dict into an old one, updating the old one (recursively).
+def dictMerge(old, new):
+    for k, v in new.iteritems():
+        if (k in old and isinstance(old[k], dict) and
+            isinstance(new[k], collections.Mapping)):
+            dictMerge(old[k], new[k])
+        else:
+            old[k] = new[k]
+
+
+#
+# TEST
+#
+if __name__ == '__main__':
+    print("TBD")
